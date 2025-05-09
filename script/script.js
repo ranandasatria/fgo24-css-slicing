67c776422d3f92044fd011a2cf3488690e68bd56
ranandasatria <rananda24@gmail.com>
2025-05-09 18:10:37 +0700

    refactor: change hamburger hover in css with  event on click on DOM

---
+++ script/script.js
@@ -0,0 +1,10 @@
+const hamburger = document.getElementById('hamburger')
+const menuTop =  document.getElementById('menuTop')
+
+hamburger.addEventListener('click', function () {
+  if (menuTop.classList.contains('active')){
+    menuTop.classList.remove('active')
+  }else {
+    menuTop.classList.add('active')
+  }
+})
